adf62a83da5c4d2587e8bfcd4222a7c45dd2ebd2
Sean Tan <sean.tan.ly@gmail.com>
2015-08-13 09:32:37 +0800

    Docs update.

---
+++ doc/sidebar_items.js
@@ -0,0 +1,16 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "Paratize"
+},{"id": "Paratize.Base",
+"docs": ["parallel_exec/2"]
+},{"id": "Paratize.Chunk",
+"docs": ["parallel_each/3","parallel_exec/2","parallel_map/3"]
+},{"id": "Paratize.Pool",
+"docs": ["parallel_each/3","parallel_exec/2","parallel_map/3"]
+},{"id": "Paratize.TaskOptions",
+"docs": ["__struct__/0"]
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
--- lib/paratize/task_options.ex
+++ lib/paratize/task_options.ex
@@ -5,7 +5,7 @@ defmodule Paratize.TaskOptions do
 
   @doc """
   * size - number of workers, default: the number of CPU cores given by `:erlang.system_info(:schedulers)`.
-  * timeout - timeout in ms, integer, default: 5000, exit(:timeout,...) if no result is return by any of the workers within the period.
+  * timeout - timeout in ms, integer, default: 5000, exit(:timeout,...) if no result is return by any of the workers within the period. To disable timeout, use `:infinity`.
   """
   defstruct size: :erlang.system_info(:schedulers), timeout: 5000
 
