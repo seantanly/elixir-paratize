58bbedeead99a99229b71d2f36a3c6452679a9b0
Sean Tan <sean.tan.ly@gmail.com>
2017-08-20 02:40:02 +0800

    Update ex_doc to be available only under dev

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Paratize.Mixfile do
     [
       {:credo, ">= 0.0.0", only: [:dev, :test]},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
-      {:ex_doc, "~> 0.16", only: [:docs], runtime: false},
+      {:ex_doc, "~> 0.16", only: [:dev], runtime: false},
     ]
   end
 
